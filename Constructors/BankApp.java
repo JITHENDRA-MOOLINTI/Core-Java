0b656a9c68446de37511ac41ea7b637481b772a6
JITHENDRA-MOOLINTI <115027059+JITHENDRA-MOOLINTI@users.noreply.github.com>
2025-05-21 11:09:11 +0530

    Add files via upload

--- Constructors/BankApp.java
+++ Constructors/BankApp.java
@@ -9,5 +9,14 @@ public class BankApp {
 	  System.out.println("----------------------------------");
 	   BankAccount b1 = new BankAccount(1000,"Alice","123 Street");
 	   b1.displayBankAccountDetails();
+	   b1.deposit(2000);
+	   b1.deposit(-1000);
+	   b1.displayBankAccountDetails();
+	   b1.withdraw(-9000);
+	   b1.withdraw(4000);
+	   b1.withdraw(200);
+	   b1.updateAddress("123 KPHB");
+	   b1.updateCustomerName("Jithendra");
+	   b1.displayBankAccountDetails();
 	}
 }
